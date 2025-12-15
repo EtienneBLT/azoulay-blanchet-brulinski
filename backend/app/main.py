69c2ce346bdb19f5cd8d8fbce54a2c909008280a
Etienne BLANCHET <etienne.blt8@gmail.com>
2025-12-15 16:42:17 +0100

    Création base du backend api users

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from sqlalchemy import text
+from app.db.session import engine, get_db
+from fastapi import Depends
+from sqlalchemy.orm import Session
 
-app = FastAPI()
+# Import des routes
+from app.routes import user_routes
 
+app = FastAPI(title="API Bibliothèque")
+
+# Inclusion des routes utilisateurs
+app.include_router(user_routes.router)
+
+# Route racine
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Bienvenue sur l'API Bibliothèque"}
